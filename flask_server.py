42114ac1ab3e0d8b26ea7229773029041514319f
Punnavit Akkarapitakchai <NPunn@client-130-203-174-93.mobility.psu.edu>
2017-04-09 05:59:32 -0400

    add wolfram

--- flask_server.py
+++ flask_server.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask import request
 from textblob import TextBlob
+import wolframalpha
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,8 +11,18 @@ def data():
 	# here we want to get the value of user (i.e. ?user=some-value)
 	text = request.args.get('text')
 	print text
+	corrected_text = str(autocor(text))
+	wolfram = callwolf(corrected_text)
 
-	return autocor(text)
+	return corrected_text + 'result: ' + wolfram
+
+def callwolf(text):
+    var_array = []
+    app_id = "2UVHV8-E7J4WKLGWG"
+    client = wolframalpha.Client(app_id)
+    res = client.query(text)
+    answer = next(res.results).text
+    return answer
 
 def autocor(text):
     b = TextBlob(text)
